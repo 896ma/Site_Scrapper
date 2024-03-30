103f86556872c7fcb8f8e2983d394a83f5c11d5e
G_oaT <solarcrash3@gmail.com>
2024-03-30 13:55:04 +0300

    chrome   versioning

--- webscraper.py
+++ webscraper.py
@@ -1,2 +1,4 @@
 from  selenium import  webdriver # import webdriver package
 url ='https://github.com/896ma'  #specify the url of  the website you want to scrap
+browser =webdriver.chrome()#Download the web browser
+browser.get(url)
