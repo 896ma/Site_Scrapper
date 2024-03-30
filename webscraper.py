3c3ec201da357b8551553b99b824222f3655465d
G_oaT <solarcrash3@gmail.com>
2024-03-30 19:14:19 +0300

    url modified

--- webscraper.py
+++ webscraper.py
@@ -1,5 +1,5 @@
 from  selenium import  webdriver # import webdriver package
-url ='https://github.com/896ma'  #specify the url of  the website you want to scrap
+url ='https://hurawatchtv.to/'  #specify the url of  the website you want to scrap
 browser =webdriver.chrome()#Download the web browser
 browser.get(url)
-browser.find_element_by_Xpath('').click() #finding  the browser element to scrap
+browser.find_element_by_Xpath('//*[@id="RxCzoB"]/script[3]').click() #finding  the browser element to scrap
