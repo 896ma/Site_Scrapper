6176403b4dd785243051a0d1440683ccfe014d61
G_oaT <solarcrash3@gmail.com>
2024-03-30 14:12:02 +0300

    browser element

--- webscraper.py
+++ webscraper.py
@@ -2,3 +2,4 @@ from  selenium import  webdriver # import webdriver package
 url ='https://github.com/896ma'  #specify the url of  the website you want to scrap
 browser =webdriver.chrome()#Download the web browser
 browser.get(url)
+browser.find_element_by_Xpath('').click() #finding  the browser element to scrap
