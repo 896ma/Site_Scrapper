48202cdbfd1c7e3efb4067e81c8770e83e4df84a
G_oaT <solarcrash3@gmail.com>
2024-03-31 00:11:37 +0300

    webdriver

--- webscraper.py
+++ webscraper.py
@@ -1,5 +1,5 @@
 from  selenium import  webdriver # import webdriver package
 url ='https://www.youtube.com/watch?v=xM1R05qHato'  #specify the url of  the website you want to scrap
-browser =webdriver.chrome()#Download the web browser
+browser =webdriver.Chrome()#Download the web browser
 browser.get(url)
 browser.find_element_by_Xpath('/html/body/link').click() #finding  the browser element to scrap
